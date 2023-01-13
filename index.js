add8620e0adb129d3b3c7634358aa0a3007d30c9
noahshotz <max.heinrich@uusimaa.de>
2023-01-13 10:26:20 +0100

    no-message

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import mysql from "mysql2";
 const app = express();
 
 const db = mysql.createConnection({
-    host: "db5011110027.hosting-data.io",
-    database: "dbs9386417",
-    user: "dbu4883651",
-    password: "#!$bhtmysqldb",
+    host: "bhtdb.chxlf7lsw2kg.eu-central-1.rds.amazonaws.com",
+    database: "bhtdb",
+    user: "admin",
+    password: "#!$mybhtdb",
     port: 3306
 });
 
@@ -17,6 +17,7 @@ app.get("/", (req, res) => {
     );
 });
 
+// access to platform table
 app.get("/platform", (req, res) => {
     const platq = "SELECT * FROM platform";
     db.query(platq, (err, data) => {
@@ -25,13 +26,24 @@ app.get("/platform", (req, res) => {
     });
 });
 
+// access to publisher table
 app.get("/publisher", (req, res) => {
-    const pubq = "SELECT * FROM publisher";
-    db.query(pubq, (err, data) => {
+    const platq = "SELECT * FROM publisher";
+    db.query(platq, (err, data) => {
         if (err) return res.json(err);
         return res.json(data);
     });
 });
 
-const PORT = process.env.PORT || 4000;
+app.get("/q5", (req, res) => {
+    const q5 = "SELECT platform.pId, CONCAT(hersteller, ' ', name) as platform, COUNT(*) AS count from platform, QUERYDATA WHERE QUERYDATA.QUERY LIKE CONCAT('% ', platform.name, '%') GROUP BY name ORDER BY count DESC"
+    db.query(q5, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+//const PORT = process.env.PORT || 4000
+
+const PORT = 8800
 app.listen(PORT, () => console.log(`Connection to backend established on port ${PORT}`));

556ba04ad6c01409783f39d7467aa6f27cb09f38
noahshotz <max.heinrich@uusimaa.de>
2023-01-18 17:30:30 +0100

    updates

--- index.js
+++ index.js
@@ -35,6 +35,23 @@ app.get("/publisher", (req, res) => {
     });
 });
 
+// acess to video games table
+app.get("/videogames", (req,res) =>  {
+    const videoq = "SELECT * from videogames LEFT JOIN platform on videogames.platform=platform.pId LIMIT 5";
+    db.query(videoq, (err, data) => {
+        if (err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.get("/q1", (req, res) => {
+    const q1 = "SELECT videogames.title AS game, COUNT(*) AS count FROM videogames, QUERYDATA WHERE QUERYDATA.QUERY LIKE '%' || videogames.title || '%' GROUP BY videogames.title LIMIT 10"
+    db.query(q1, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
 app.get("/q5", (req, res) => {
     const q5 = "SELECT platform.pId, CONCAT(hersteller, ' ', name) as platform, COUNT(*) AS count from platform, QUERYDATA WHERE QUERYDATA.QUERY LIKE CONCAT('% ', platform.name, '%') GROUP BY name ORDER BY count DESC"
     db.query(q5, (err, data) => {

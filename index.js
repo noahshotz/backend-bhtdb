d6d3d9f4b8212855397e4130d89f549e34c04ac8
noahshotz <max.heinrich@uusimaa.de>
2023-01-27 09:59:10 +0100

    updating port

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ app.get("/q5", (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Connection to backend established on port ${PORT}`));
